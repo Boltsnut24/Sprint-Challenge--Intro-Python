508c96ab1522fbb95f70ad5dc33f95d28b909914
Brad <Brad@BRADSPC>
2020-05-08 11:09:59 -0600

    Fixed test by typecasting coords to float and skipped first index of csv

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -14,13 +14,29 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv
+
+class City():
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return F"{self.name} is at ({self.lat}, {self.lon})"
+
 cities = []
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  with open('cities.csv', 'r') as csvfile:
+    cityDataRaw = csv.reader(csvfile)
+
+    next(cityDataRaw, None) #skips first index
+    for row in cityDataRaw:
+      cities.append(City(row[0], float(row[3]), float(row[4])))
     return cities
 
 cityreader(cities)
